3755a18aeb2e023e10fe6e094813920931f61fa7
haden <80521474+thdefn@users.noreply.github.com>
2023-04-01 14:32:51 +0900

    jpa 방식으로 데이터 저장하기

---
+++ src/test/java/zerobase/weather/JpaMemoRepositoryTest.java
@@ -0,0 +1,43 @@
+package zerobase.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import zerobase.weather.domain.Memo;
+import zerobase.weather.repository.JpaMemoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class JpaMemoRepositoryTest {
+    @Autowired
+    JpaMemoRepository jpaMemoRepository;
+
+    @Test
+    void insertMemoTest() {
+        //given
+        Memo newMemo = new Memo(10, "this is jpa memo");
+        //when
+        jpaMemoRepository.save(newMemo); // 그 어떠한 함수도 구현하지 않았지만 jpa가 다 구현해줌
+        //then
+        List<Memo> memos = jpaMemoRepository.findAll();
+        assertTrue(memos.size() > 0);
+    }
+
+    @Test
+    void findByIdTest() {
+        //given
+        Memo newMemo = new Memo(11, "jpa"); // 아이디를 11로 지정해도 GenerationType.IDENTITY 정책 때문에 반영되지 않는다
+        //when
+        Memo memo = jpaMemoRepository.save(newMemo);
+        System.out.println(memo.getId());
+        //then
+        Optional<Memo> result = jpaMemoRepository.findById(memo.getId());
+        assertEquals(result.get().getText(), newMemo.getText());
+    }
+}
