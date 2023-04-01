10e4868fbc6c0c196b92e5b8bdd13e57d8959398
haden <80521474+thdefn@users.noreply.github.com>
2023-04-01 20:35:10 +0900

    날씨 데이터 저장 api 작성

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -1,5 +1,8 @@
 package zerobase.weather.service;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -8,11 +11,14 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service // 어노테이션을 붙여주지 않으면 스프링 부트가 서비스 클래스임을 인식하지 못하고 빈으로 등록도 안됨
 public class DiaryService {
 
-    @Value("${openweathermap.key}") // 실무의 다양한 환경들이 ex> real/test/dev 같은 DB를 바라보지 않아서 profile을 사용하기 위함
+    @Value("${openweathermap.key}")
+    // 실무의 다양한 환경들이 ex> real/test/dev 같은 DB를 바라보지 않아서 profile을 사용하기 위함
     private String apiKey;
 
     // 코드의 재사용성과 가독성을 위해 각각의 기능들을 함수로 분리
@@ -20,7 +26,7 @@ public class DiaryService {
         // open weather map 에서 날씨 데이터 받아오기
         String weatherData = getWeatherString();
         // 받아온 날씨 json 파싱하기
-
+        Map<String, Object> parsedWeather = parseWeather(weatherData);
         // 파싱된 데이터 + 일기 값 db에 넣기
     }
 
@@ -40,13 +46,37 @@ public class DiaryService {
             }
             String inputLine;
             StringBuilder response = new StringBuilder();
-            while ((inputLine = br.readLine()) != null ){
+            while ((inputLine = br.readLine()) != null) {
                 response.append(inputLine);
             }
             br.close();
+
             return response.toString();
+
         } catch (Exception e) {
             return "failed to get response";
+        }
+    }
+
+    // {"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01n"}],
+    // "main":{"temp":291.88,"feels_like":290.62,"temp_min":283.84,"temp_max":295.81,"pressure":1014,"humidity":31}}
+    private Map<String, Object> parseWeather(String jsonString) {
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject;
+
+        try {
+            jsonObject = (JSONObject) jsonParser.parse(jsonString);
+            Map<String, Object> resultMap = new HashMap<>();
+
+            JSONObject mainData = (JSONObject) jsonObject.get("main");
+            JSONObject weatherData = (JSONObject) jsonObject.get("weather");
+            resultMap.put("temp", mainData.get("temp"));
+            resultMap.put("main", weatherData.get("main"));
+            resultMap.put("icon", weatherData.get("icon"));
+            return resultMap;
+
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
         }
     }
 }
