5b3c6ed28787346058344c25252411983ee69cc1
haden <80521474+thdefn@users.noreply.github.com>
2023-04-01 19:30:43 +0900

    날씨 데이터 저장 api 작성

--- src/main/java/zerobase/weather/controller/DiaryController.java
+++ src/main/java/zerobase/weather/controller/DiaryController.java
@@ -2,15 +2,15 @@ package zerobase.weather.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import zerobase.weather.service.DiaryService;
 
 import java.time.LocalDate;
 
-@Controller // 스프링이 컨트롤러로 인식하게 함
+@RestController // RestController는 400 200 등의 status code를 지정해서 내려줄 수 있게 함
 public class DiaryController {
 
     private final DiaryService diaryService;
@@ -20,10 +20,11 @@ public class DiaryController {
         this.diaryService = diaryService;
     }
 
-    @PostMapping("/create/diary") // 저장할때는 포스트
+    @PostMapping("/create/diary")
+        // 저장할때는 포스트
     void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
                      @RequestBody String text) {
-       diaryService.createDiary(date, text);
+        diaryService.createDiary(date, text);
     }
 
 }
--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -1,12 +1,52 @@
 package zerobase.weather.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDate;
 
 @Service // 어노테이션을 붙여주지 않으면 스프링 부트가 서비스 클래스임을 인식하지 못하고 빈으로 등록도 안됨
 public class DiaryService {
-    public void createDiary(LocalDate date, String text) {
 
+    @Value("${openweathermap.key}") // 실무의 다양한 환경들이 ex> real/test/dev 같은 DB를 바라보지 않아서 profile을 사용하기 위함
+    private String apiKey;
+
+    // 코드의 재사용성과 가독성을 위해 각각의 기능들을 함수로 분리
+    public void createDiary(LocalDate date, String text) {
+        // open weather map 에서 날씨 데이터 받아오기
+        String weatherData = getWeatherString();
+        // 받아온 날씨 json 파싱하기
+
+        // 파싱된 데이터 + 일기 값 db에 넣기
+    }
+
+    private String getWeatherString() {
+        String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=" + apiKey;
+
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+            BufferedReader br;
+            if (responseCode == 200) {
+                br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            } else {
+                br = new BufferedReader(new InputStreamReader(connection.getErrorStream())); // 에러를 가져온다
+            }
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = br.readLine()) != null ){
+                response.append(inputLine);
+            }
+            br.close();
+            return response.toString();
+        } catch (Exception e) {
+            return "failed to get response";
+        }
     }
 }
